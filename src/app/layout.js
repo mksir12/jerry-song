9afac306a09297009b4ce9a061c0191e16b1bab8
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2024-05-25 13:31:25 +0000

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,14 +17,14 @@ const poppins = Poppins({
 });
 
 export const metadata = {
-  title: "Hayasaka",
+  title: "Musio",
   description: "Music streaming app",
   image:
-    "https://res.cloudinary.com/dbr73rpz9/image/upload/v1690380865/images/logo-color_noktgr.png",
-  url: "https://hayasaka.vercel.app",
+    "https://res.cloudinary.com/ddhg99aub/image/upload/v1716643815/b0qaj44bpeio6declavp.png",
+  url: "https://musio-ten.vercel.app",
   type: "website",
   icons: [{ rel: "icon", url: Favicon.src }],
-  site_name: "Hayasaka",
+  site_name: "Musio",
   manifest: "/manifest.json",
 };
 
